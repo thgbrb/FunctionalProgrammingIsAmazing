58f6304f2e995f7d22bb040c16a1101fc0367734
Jonas Luz <jonasluz85@hotmail.com>
2017-07-22 11:36:44 -0300

    Windows Form para processar um arquivo .sql

---
+++ QueryScoreBoard/QueryScoreBoard.Interface/frmSqlAnalyzer.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace QueryScoreBoard.Presentation
+{
+    partial class frmSqlAnalyzer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialogSql = new System.Windows.Forms.OpenFileDialog();
+            this.btnAnalyse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialogSql
+            // 
+            this.openFileDialogSql.Filter = "*.sql|(*.sql)";
+            // 
+            // btnAnalyse
+            // 
+            this.btnAnalyse.Location = new System.Drawing.Point(12, 12);
+            this.btnAnalyse.Name = "btnAnalyse";
+            this.btnAnalyse.Size = new System.Drawing.Size(422, 23);
+            this.btnAnalyse.TabIndex = 0;
+            this.btnAnalyse.Text = "Analisar SQL...";
+            this.btnAnalyse.UseVisualStyleBackColor = true;
+            this.btnAnalyse.Click += new System.EventHandler(this.btnAnalyse_Click);
+            // 
+            // frmSqlAnalyzer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 48);
+            this.Controls.Add(this.btnAnalyse);
+            this.MaximizeBox = false;
+            this.Name = "frmSqlAnalyzer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sql Analyzer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialogSql;
+        private System.Windows.Forms.Button btnAnalyse;
+    }
+}
+
