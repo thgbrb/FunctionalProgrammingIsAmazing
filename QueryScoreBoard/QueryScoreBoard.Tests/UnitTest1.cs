027e8174a6040a6f60f2f4c66dd948e9f0c27066
Thiago Borba <thiago.borba@icloud.com>
2017-07-22 12:26:01 -0300

    Alteração do proejto de teste para Microsoft Tests

---
+++ QueryScoreBoard/QueryScoreBoard.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+
+namespace QueryScoreBoard.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TDD()
+        {
+            var teste = $@"Select * from Alguma coisa Where teste.Id = ""Teste"" ";
+
+            var pattern = "Select";
+
+            var regex = new Regex(string.Format(@"\b{0}\b", teste), RegexOptions.IgnoreCase);
+
+            var count = regex.Matches(teste).Count;
+
+            Assert.AreEqual(1, count);
+        }
+    }
+}
