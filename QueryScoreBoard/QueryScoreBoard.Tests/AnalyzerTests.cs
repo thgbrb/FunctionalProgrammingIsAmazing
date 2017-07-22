f2b61c6a7face9318584dcc7484ba41026b0920c
Thiago Borba <thiago.borba@icloud.com>
2017-07-22 11:55:06 -0300

    Projeto de teste e Analyzer

---
+++ QueryScoreBoard/QueryScoreBoard.Tests/AnalyzerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace QueryScoreBoard.Tests
+{
+    public class AnalyzerTests
+    {
+        [Fact]
+        public void TDD()
+        {
+            var teste = $@"Select * from Alguma coisa Where teste.Id = ""Teste"" ";
+
+            var pattern = $@"";
+
+            //Regex.Match(teste, )
+        }
+    }
+}
